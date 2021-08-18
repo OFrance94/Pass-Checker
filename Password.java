9fcc053223474592706f9aceff99ea1b60214421
OFrance94 <53825440+OFrance94@users.noreply.github.com>
2021-08-18 15:09:58 +0100

    Seperated parts into methods to clean up code, namely password check and password input

--- Password.java
+++ Password.java
@@ -2,58 +2,53 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Password 
-{
-
-	public static void main(String[] args) 
-	{
-		List<String> passInput = new LinkedList<String>();
-		boolean arraycheck = false;
-		Scanner pInput = new Scanner(System.in);
-			
-		System.out.println("Please enter 6 passwords.");	
+public class Password {
+	static List<String> passInput = new LinkedList<String>();
+	static boolean arraycheck = false;
+	static Scanner pInput = new Scanner(System.in);
+	
+	public static void main(String[] args) { // TRY USING HASHMAP?
 		
-		for(int i=5; i>=0; i--) {	
-			String userinput = pInput.nextLine();
-			passInput.add(userinput);
-			}
-			
+		passwordInput();		
+		passCheck();
 		pInput.close();
 
+	}
+	
+	public static void passwordInput() {
 		
-		
+		System.out.println("Please enter 6 passwords.");
+
+		for (int i = 5; i >= 0; i--) {
+			String userinput = pInput.nextLine();
+			passInput.add(userinput);
+		}
+	}
+	
+	public static void passCheck() {
 		for (String s : passInput) {
 			char[] solocheck = s.toCharArray();
-			
+
 			for (Character sc : solocheck) {
-				if(Character.isDigit(sc)) {
+				if (Character.isDigit(sc)) {
 					arraycheck = true;
 					break;
 				}
-				
+
 			}
-			if (arraycheck == true && s.length() > 8){
-				System.out.println("Array Password " + s +" is Very Strong!");
-			}
-			else if(arraycheck == true && s.length() >= 5){
+			if (arraycheck == true && s.length() > 8) {
+				System.out.println("Array Password " + s + " is Very Strong!");
+			} else if (arraycheck == true && s.length() >= 5) {
 				System.out.println("Array Password " + s + "  Strong!");
-			}
-			else if(arraycheck== true && s.length() < 5) {
-				System.out.println("Array Password " + s +" needs to be longer.");
-			}
-			else if(s.length() >=8 && !(arraycheck==true)) {
-				System.out.println("Array Password " + s + " has good password length, but needs numbers.");				
-			}
-			else if(s.length() >=5 && !(arraycheck==true)) {
+			} else if (arraycheck == true && s.length() < 5) {
+				System.out.println("Array Password " + s + " needs to be longer.");
+			} else if (s.length() >= 8 && !(arraycheck == true)) {
+				System.out.println("Array Password " + s + " has good password length, but needs numbers.");
+			} else if (s.length() >= 5 && !(arraycheck == true)) {
 				System.out.println("Array Password " + s + " should be longer and needs numbers.");
-			}
-			else {
+			} else {
 				System.out.println("Array password " + s + " invalid.");
 			}
 		}
-		
-		
-	
 	}
-
 }
